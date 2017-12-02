bfbdb8c8256f70808f63fbecdc4e5c19657e2ac2
Alicia Good <agood@opentext.com>
2017-12-01 23:29:39 -0600

    Pushing finished program code - without deployment to heroku

---
+++ server3.js
@@ -0,0 +1,60 @@
+// @comment: Homework 18 - Web Scraper with Express, Handlebars, MongoDB and Cheerio
+
+
+// Node Dependencies
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var logger = require('morgan'); // for debugging
+var request = require('request'); // for web-scraping
+var cheerio = require('cheerio'); // for web-scraping
+
+// Initialize Express for debugging & body parsing
+var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger('dev'));
+
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+// Use express.static to serve the public folder as a static directory
+
+app.use(express.static("public"));
+
+// Express-Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// Database Configuration with Mongoose
+// ---------------------------------------------------------------------------------------------------------------
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+  mongoose.Promise = Promise;
+  mongoose.connect('mongodb://localhost/news-goose', {
+  	useMongoClient: true
+  });
+
+var db = require('./models/comment.js');
+
+// Import the Comment and Article models
+var Comment = require('./models/comment.js');
+var Article = require('./models/article.js');
+// ---------------------------------------------------------------------------------------------------------------
+
+// Import Routes/Controller
+var router = require('./controllers/controller.js');
+app.use('/', router);
+
+
+// Launch App
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Running on port: ' + port);
+});
